7b6e6242d1ec951951138480ca22ac05a93996a8
vaibhav-chaudhari6696 <84838129+vaibhav-chaudhari6696@users.noreply.github.com>
2023-02-24 17:13:57 +0530

    New Update

--- windowscript.sh
+++ windowscript.sh
@@ -5,23 +5,35 @@ REMOTE_HOST=192.168.2.100
 REMOTE_USER=vaibhav
 
 # Define the command to run on the remote host
-REMOTE_COMMAND='wmic os list brief'
-
+REMOTE_COMMAND='wmic os get Caption,Version,BuildNumber,OSArchitecture,SerialNumber'
 # Connect to the remote host and run the command
 info1=$(ssh "$REMOTE_USER@$REMOTE_HOST" "$REMOTE_COMMAND")
 
-REMOTE_COMMAND='WMIC LOGICALDISK where drivetype!=4 get deviceid, description, volumename'
+REMOTE_COMMAND='wmic nicconfig get description, IPAddress, MACAddress'
 info2=$(ssh "$REMOTE_USER@$REMOTE_HOST" "$REMOTE_COMMAND")
 
-REMOTE_COMMAND='WMIC BIOS get serialnumber'
+REMOTE_COMMAND='wmic logicaldisk get deviceid,volumename,size,filesystem'
 info3=$(ssh "$REMOTE_USER@$REMOTE_HOST" "$REMOTE_COMMAND")
+
+
+REMOTE_COMMAND='wmic memorychip get capacity,manufacturer,speed,partnumber'
+info4=$(ssh "$REMOTE_USER@$REMOTE_HOST" "$REMOTE_COMMAND")
+
+REMOTE_COMMAND='wmic cpu get name,NumberOfCores,NumberOfLogicalProcessors,MaxClockSpeed'
+info5=$(ssh "$REMOTE_USER@$REMOTE_HOST" "$REMOTE_COMMAND")
 
 echo "$info1"
 echo "$info2"
 echo "$info3"
+echo "$info4"
+echo "$info5"
+
 echo "$info1" > "$REMOTE_HOST.txt"
 echo "$info2" >> "$REMOTE_HOST.txt"
 echo "$info3" >> "$REMOTE_HOST.txt"
+echo "$info4" >> "$REMOTE_HOST.txt"
+echo "$info5" >> "$REMOTE_HOST.txt"
+
 
 
 
